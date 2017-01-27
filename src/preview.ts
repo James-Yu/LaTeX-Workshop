06d58f442f2c1ae4faf084e7bd92cbdd5eee499d
James-Yu <jqyu@eee.hku.hk>
2017-01-27 14:42:30 +0800

    Further fix relative path detection

--- src/preview.ts
+++ src/preview.ts
@@ -115,7 +115,7 @@ async function onClientMessage(msg) {
             let pos = new vscode.Position(row, col);
 
             let file = record["input"].replace(/(\r\n|\n|\r)/gm, "");
-            if (file.charAt(0) != '/')
+            if (file.charAt(0) != '/' && file.indexOf(':\\') < 0)
                 file = path.join(path.dirname(latex_data.main_document), file);
             file = file.replace(/(\/|\\).(\/|\\)/gm, "\/");
             let doc = await vscode.workspace.openTextDocument(file);
