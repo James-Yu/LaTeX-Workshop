ae1a7f96cbe33715abbe442e91d5431de3ff6930
James-Yu <jqyu@eee.hku.hk>
2017-04-03 19:27:08 +0800

    A more stable spell checker library

--- src/checker.ts
+++ src/checker.ts
@@ -1,32 +1,26 @@
 import * as vscode from 'vscode'
-import * as fs from 'fs'
-import * as spellchecker from 'hunspell-spellchecker'
+import * as spellchecker from 'simple-spellchecker'
 
 import {Extension} from './main'
 
 export class Checker {
     extension: Extension
     errors: {[id: string]: [[{from: number, to: number, text: string} | undefined]] | never[]} = {}
-    spell = new spellchecker()
     checkerTimeout: NodeJS.Timer
     checkerDiagnostics = vscode.languages.createDiagnosticCollection('Spell Check')
+    spell
 
     constructor(extension: Extension) {
         this.extension = extension
-        const affFile = fs.readFileSync(`${__dirname}/../../dicts/en_US.aff`)
-        const dicFile = fs.readFileSync(`${__dirname}/../../dicts/en_US.dic`)
-        console.log(2)
-        const dict = this.spell.parse({
-            aff: affFile,
-            dic: dicFile
-        })
-        console.log(3)
-        this.spell.use(dict)
+        this.spell = spellchecker.getDictionarySync('en-US', `${__dirname}/../../dicts`)
+        this.spell.check = this.spell.spellCheck
         this.extension.logger.addLogMessage(`Dictionary files loaded`)
     }
 
     check(doc: vscode.TextDocument, line: number | undefined = undefined) {
-        this.extension.logger.addLogMessage(`Spell Check Invoked`)
+        if (!this.spell) {
+            return
+        }
         if (!this.errors[doc.fileName]) {
             this.errors[doc.fileName] = []
         }
@@ -46,15 +40,15 @@ export class Checker {
             content = this.remove(content, /[a-zA-Z.\-0-9]+@[a-z.]+/g)
             content = this.remove(content, /(http|https|ftp|git)\S*/g)
             content = this.remove(content, /\(.*\.(jpg|jpeg|png|md|gif|pdf|svg)\)/gi)
-            content = this.remove(content, /[`\"!#$%&()*+,.\/:;<=>?@\[\]\\^_{|}\n\r\-~]/g)
+            content = this.remove(content, /[`\"!#$%&()*+,.\/:;<=>?-]/g)
             content = this.remove(content, / [0-9]+/g)
             content = this.remove(content, /\t/g)
             content = this.remove(content, /' /g)
 
             content = content.replace(/[\s]['"]([a-zA-Z0-9])/g, ' $1')
             this.errors[doc.fileName][realLineIdx] = [ undefined ]
-            content.split(' ').forEach(token => {
-                if (token.length < 4 || this.spell.check(token)) {
+            content.split(/[\s']/).forEach(token => {
+                if (token.length < 4 || /[@\[\]\\^_{|}\n\r\~]/.exec(token) || this.spell.check(token)) {
                     return
                 }
                 const colNum = lines[lineIdx].indexOf(token)
