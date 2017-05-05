69142fac6e1cd9a5192317b13ae832748c97a42f
James-Yu <jqyu@eee.hku.hk>
2017-05-05 14:08:08 +0800

    (#127) Provide context hint in reference intellisense document

--- src/providers/reference.ts
+++ src/providers/reference.ts
@@ -40,6 +40,7 @@ export class Reference {
         Object.keys(suggestions).map(key => {
             const item = suggestions[key]
             const command = new vscode.CompletionItem(item.reference, vscode.CompletionItemKind.Reference)
+            command.documentation = item.text
             this.suggestions.push(command)
         })
         return this.suggestions
@@ -58,8 +59,11 @@ export class Reference {
                 break
             }
             if (!(result[1] in items)) {
+                const prevContent = content.substring(0, content.substring(0, result.index).lastIndexOf('\n') - 1)
+                const followLength = content.substring(result.index, content.length).split('\n', 3).join('\n').length
                 items[result[1]] = {
-                    reference: result[1]
+                    reference: result[1],
+                    text: `${content.substring(prevContent.lastIndexOf('\n'), result.index + followLength)}\n...`
                 }
             }
         }
