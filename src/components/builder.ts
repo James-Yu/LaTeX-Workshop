1765e98a3b3265eb54de5c5a45bb4cbebfacd6ee
Álan Crístoffer <acristoffers@gmail.com>
2018-04-07 08:25:20 -0300

    Fix #519 - Running bibles on projects without bib
    If there’s no magic bib command, it will not run the bibtex phase.

--- src/components/builder.ts
+++ src/components/builder.ts
@@ -142,7 +142,11 @@ export class Builder {
                 command: magicBib,
                 args: configuration.get('latex.magic.bib.args') as string[]
             }
-            steps = [magicTexStep, magicBibStep, magicTexStep, magicTexStep]
+            if (magicBib === '') {
+                steps = [magicTexStep, magicTexStep, magicTexStep]
+            } else {
+                steps = [magicTexStep, magicBibStep, magicTexStep, magicTexStep]
+            }
         } else {
             const recipes = configuration.get('latex.recipes') as {name: string, tools: (string | StepCommand)[]}[]
             const tools = configuration.get('latex.tools') as StepCommand[]
@@ -191,7 +195,7 @@ export class Builder {
         const tex = content.match(regexTex)
         const bib = content.match(regexBib)
         let texProgram = ''
-        let bibProgram = 'bibtex'
+        let bibProgram = ''
 
         if (tex) {
             texProgram = tex[1]
