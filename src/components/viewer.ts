664c6e1335d316938ef311ea8917f8fe5bde44f9
lizz <363664470@qq.com>
2018-07-01 17:56:43 +0800

    don't lose focus for pdf tab preview

--- src/components/viewer.ts
+++ src/components/viewer.ts
@@ -96,7 +96,13 @@ export class Viewer {
             client.websocket.close()
         }
         this.clients[pdfFile.toLocaleUpperCase()] = {type: 'tab'}
-        vscode.commands.executeCommand('vscode.previewHtml', uri, column, path.basename(pdfFile))
+        const window = vscode.window.activeTextEditor
+        vscode.commands.executeCommand('vscode.previewHtml', uri, column, path.basename(pdfFile)).then(success => {
+            if (success && window) {
+                vscode.window.showTextDocument(window.document)
+            }
+            return true
+        })
         this.extension.logger.addLogMessage(`Open PDF tab for ${pdfFile}`)
     }
 
