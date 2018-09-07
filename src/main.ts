c2be9c3b7c9527a93f1672e4982ecb61d8023922
James-Yu <yujianqiaojames@gmail.com>
2018-09-07 16:19:42 +0800

    Remove previous config migration

--- src/main.ts
+++ src/main.ts
@@ -40,37 +40,6 @@ function obsoleteConfigCheck() {
             configuration.update(originalConfig, undefined, false)
         }
     }
-    renameConfig('latex.autoBuild.enabled', 'latex.autoBuild.onSave.enabled')
-    renameConfig('viewer.zoom', 'view.pdf.zoom')
-    renameConfig('viewer.hand', 'view.pdf.hand')
-    renameConfig('debug.showUpdateMessage', 'message.update.show')
-    if (configuration.has('version')) {
-        configuration.update('version', undefined, true)
-    }
-    if (configuration.has('latex.toolchain') && !configuration.has('latex.recipes')) {
-        vscode.window.showWarningMessage(`LaTeX Workshop has updated its original toolchain system to a new recipe system. Please change your "latex-workshop.latex.toolchain" setting.`,
-            'Auto-change', 'More info', 'Close')
-        .then(option => {
-            switch (option) {
-                case 'Auto-change':
-                    const toolchain = (configuration.get('latex.toolchain') as {name: string, command: string}[]).map((tool, idx) => {
-                        tool.name = `Step ${idx + 1}: ${tool.command}`
-                        return tool
-                    })
-                    configuration.update('latex.tools', toolchain, true)
-                    configuration.update('latex.recipes', [{name: 'toolchain', tools: toolchain.map(tool => tool.name)}], true)
-                    configuration.update('latex.toolchain', undefined, true)
-                    vscode.window.showInformationMessage(`A new recipe named "toolchain" is created. Please double check if it is correctly migrated in the configuration.`)
-                    break
-                case 'More info':
-                    vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(
-                        'https://github.com/James-Yu/LaTeX-Workshop#recipe'))
-                    break
-                case 'Close':
-                default:
-            }
-        })
-    }
 }
 
 function conflictExtensionCheck() {
