7a4225251afe57c59bbb7f7b17d6f8f1429274bf
James-Yu <jqyu@eee.hku.hk>
2018-11-17 23:26:06 +0800

    Resolve #989 Include package commands from cwl files

---
+++ dev/pkgcommand.py
@@ -0,0 +1,66 @@
+import json
+import urllib.request
+import zipfile
+from os import listdir, remove
+from os.path import isfile, join
+
+commands = json.load(open('../data/commands.json'))
+envs = json.load(open('../data/environments.json'))
+urllib.request.urlretrieve('https://github.com/LaTeXing/LaTeX-cwl/archive/master.zip', 'cwl.zip')
+zip_ref = zipfile.ZipFile('cwl.zip', 'r')
+zip_ref.extractall('cwl/')
+zip_ref.close()
+remove('cwl.zip')
+
+cwl_files = []
+for f in listdir('cwl/LaTeX-cwl-master'):
+    if isfile(join('cwl/LaTeX-cwl-master', f)) and f[-4:] == '.cwl':
+        cwl_files.append(f)
+    else:
+        remove(join('cwl/LaTeX-cwl-master', f))
+
+for cwl_file in cwl_files:
+    with open(join('cwl/LaTeX-cwl-master', cwl_file)) as f:
+        lines = f.readlines()
+    for line in lines:
+        if line[0] == '#':
+            continue
+        if line[:7] == '\\begin{':
+            env = line[line.index('{') + 1:line.index('}')]
+            if env in envs:
+                continue
+            envs.append(env)
+        if line[:5] == '\\end{':
+            continue
+        if line[0] != '\\':
+            continue
+        try:
+            curly = line.index('{')
+        except:
+            curly = 10000
+        try:
+            squared = line.index('[')
+        except:
+            squared = 10000
+        try:
+            hash_ = line.index('#')
+        except:
+            hash_ = 10000
+        if min(curly, squared, hash_) == 10000:
+            command = line[1:]
+        else:
+            command = line[1:min(curly, squared, hash_)]
+        if command[-1] == '\n':
+            command = command[:-1]
+        if command in commands:
+            continue
+        command_dict = { 'command': command }
+        if line.count('{') > 0:
+            command_dict['snippet'] = command + \
+                ''.join(['{${' + str(index + 1) + '}}' for index in range(line.count('{'))])
+        commands[command] = command_dict
+
+    remove(join('cwl/LaTeX-cwl-master', cwl_file))
+
+json.dump(envs, open('../data/environments.json', 'w'), indent=2)
+json.dump(commands, open('../data/commands.json', 'w'), indent=2)
