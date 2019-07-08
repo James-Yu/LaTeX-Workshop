4cbbb612c4eb38471d81c0ca81d65e97528a17dd
Trevor Gunn <tgunn@live.ca>
2019-07-08 15:20:43 -0400

    Allow all printable ASCII characters in paths.

--- src/components/parser.ts
+++ src/components/parser.ts
@@ -257,8 +257,8 @@ export class Parser {
         if (result && result.index !== undefined && result.index > -1) {
             line = line.substr(result.index + 1)
             if (result[1] === '(') {
-                const pathResult = line.match(/^"?((?:(?:[a-zA-Z]:|\.|\/)?(?:\/|\\\\?))[\w\u00a1-\uffff\-. \/\\#]*)/)
-                const mikTeXPathResult = line.match(/^"?([\w\u00a1-\uffff\-\/. #]*\.[a-z]{3,})/)
+                const pathResult = line.match(/^"?((?:(?:[a-zA-Z]:|\.|\/)?(?:\/|\\\\?))[\u0020-\u007E\u00a1-\uffff]*)/)
+                const mikTeXPathResult = line.match(/^"?([\u0020-\u007E\u00a1-\uffff]*\.[a-z]{3,})/)
                 if (pathResult) {
                     fileStack.push(pathResult[1].trim())
                 } else if (mikTeXPathResult) {
