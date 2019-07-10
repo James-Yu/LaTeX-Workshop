9bf8fe5230e5a087562081adbd28473b8d347f18
Takashi Tamura <tamuratak@users.noreply.github.com>
2019-07-10 09:14:02 +0900

    add src/components/encodePath.ts

---
+++ src/components/encodePath.ts
@@ -0,0 +1,15 @@
+export const pdfFilePrefix = 'pdf..'
+
+export function encodePath(path: string) {
+    const s = encodeURIComponent(path)
+    const b64 = Buffer.from(s).toString('base64')
+    const b64url = b64.replace(/\+/g, '-').replace(/\//g, '_').replace(/=/, '')
+    return b64url
+}
+
+export function decodePath(b64url: string) {
+    const tmp = b64url + '='.repeat((4 - b64url.length % 4) % 4)
+    const b64 = tmp.replace(/-/g, '+').replace(/_/g, '/')
+    const s = Buffer.from(b64, 'base64').toString()
+    return decodeURIComponent(s)
+}
