9db06897947b83120ec92b3f6c6174123f625d96
Trevor Gunn <tgunn@live.ca>
2019-07-11 21:52:21 -0400

    Multiple tags now work in folding

--- src/providers/folding.ts
+++ src/providers/folding.ts
@@ -9,7 +9,7 @@ export class FoldingProvider implements vscode.FoldingRangeProvider {
     constructor(extension: Extension) {
         this.extension = extension
         const sections = vscode.workspace.getConfiguration('latex-workshop').get('view.outline.sections') as string[]
-        this.sectionRegex = sections.map(section => RegExp(`\\\\${section}(?:\\*)?(?:\\[[^\\[\\]\\{\\}]*\\])?{(.*)}`, 'm'))
+        this.sectionRegex = sections.map(section => RegExp(`\\\\(?:${section})(?:\\*)?(?:\\[[^\\[\\]\\{\\}]*\\])?{(.*)}`, 'm'))
     }
 
     public provideFoldingRanges(
