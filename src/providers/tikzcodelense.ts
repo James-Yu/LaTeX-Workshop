7a1871543477ea89d0daf1d281be67ab2ea9a030
tecosaur <20903656+tecosaur@users.noreply.github.com>
2019-07-20 11:13:24 +0800

    Fix tikzpicture bug

--- src/providers/tikzcodelense.ts
+++ src/providers/tikzcodelense.ts
@@ -53,7 +53,7 @@ function findTikzPictures(document: vscode.TextDocument) {
             line = document.lineAt(++lineNo)
             text = line.text.substr(0, 1000)
             endMatch = text.match(endRegex)
-            if (endMatch && endMatch.index) {
+            if (endMatch && endMatch.index !== undefined) {
                 endColumn = endMatch.index + endMatch[0].length
             }
         } while (!endMatch)
