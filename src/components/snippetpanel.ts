ad93623fd2c4f1ad9bab673d50cf824504257a71
tecosaur <20903656+tecosaur@users.noreply.github.com>
2019-07-21 11:11:55 +0800

    Add dev/ file for symbol svg creation

--- src/components/snippetpanel.ts
+++ src/components/snippetpanel.ts
@@ -30,30 +30,6 @@ export class SnippetPanel {
                 this.lastActiveTextEditor = textEditor
             }
         })
-
-        import('mathjax-node')
-            .then(mj => {
-                this.mathJax = mj
-                mj.config({
-                    MathJax: {
-                        jax: ['input/TeX', 'output/SVG'],
-                        extensions: ['tex2jax.js', 'MathZoom.js'],
-                        showMathMenu: false,
-                        showProcessingMessages: false,
-                        messageStyle: 'none',
-                        SVG: {
-                            useGlobalCache: false
-                        },
-                        TeX: {
-                            extensions: ['AMSmath.js', 'AMSsymbols.js', 'autoload-all.js', 'color.js', 'noUndefined.js']
-                        }
-                    }
-                })
-                mj.start()
-            })
-            .then(() => {
-                this.loadSnippets()
-            })
     }
 
     public async showPanel() {
@@ -125,65 +101,16 @@ export class SnippetPanel {
             };
         } = JSON.parse(readFileSync(snipetsFile, { encoding: 'utf8' }))
 
-        const mathSymbolPromises: Promise<any>[] = []
         for (const category in snippets.mathSymbols) {
             for (let i = 0; i < snippets.mathSymbols[category].length; i++) {
                 const symbol = snippets.mathSymbols[category][i]
-                if (symbol.svg === undefined) {
-                    mathSymbolPromises.push(
-                        new Promise((resolve, reject) => {
-                            this.mathJax
-                                .typeset({
-                                    math: symbol.source,
-                                    format: 'TeX',
-                                    svgNode: true
-                                })
-                                .then(
-                                    (data: {
-                                        height: string;
-                                        speakText: string;
-                                        style: string;
-                                        svgNode: SVGSVGElement;
-                                        width: string;
-                                    }) => {
-                                        let svg = data.svgNode.outerHTML
-                                        svg = svg.replace(
-                                            /<title([^>]*)>(.*)<\/title>/,
-                                            `<title$1>${symbol.name.toLocaleUpperCase()}.${
-                                                symbol.keywords ? ' Keywords: ' + symbol.keywords : ''
-                                            }</title>`
-                                        )
-                                        if (symbol.shrink) {
-                                            svg = svg.replace(/^<svg/, '<svg class="shrink"')
-                                        }
-                                        symbol.svg = svg
-                                        resolve()
-                                    }
-                                )
-                                .catch(reject)
-                        })
-                    )
+                symbol.category = category
+                if (symbol.keywords === undefined) {
+                    symbol.keywords = ''
                 }
+                this.mathSymbols.push(symbol)
             }
         }
-        Promise.all(mathSymbolPromises).finally(() => {
-            if (mathSymbolPromises.length > 0) {
-                writeFileSync(snipetsFile, JSON.stringify(snippets, undefined, 4))
-                vscode.window.showInformationMessage(
-                    `LaTeX-Workshop: ${mathSymbolPromises.length} symbols rendered and cached`
-                )
-            }
-            for (const category in snippets.mathSymbols) {
-                for (let i = 0; i < snippets.mathSymbols[category].length; i++) {
-                    const symbol = snippets.mathSymbols[category][i]
-                    symbol.category = category
-                    if (symbol.keywords === undefined) {
-                        symbol.keywords = ''
-                    }
-                    this.mathSymbols.push(symbol)
-                }
-            }
-        })
     }
 
     private async initialisePanel() {
