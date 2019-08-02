5d50f1e810e59c41d599b8613e689ad40bffe186
Takashi Tamura <tamuratak@users.noreply.github.com>
2019-08-02 20:34:35 +0900

    run eslint for viewr/latexworkshop.js

--- viewer/latexworkshop.js
+++ viewer/latexworkshop.js
@@ -31,7 +31,7 @@ class ViewerHistory {
 
   set(scroll, force = false) {
     if (this._history.length === 0) {
-      this._history.push({scroll: scroll, temporary: false})
+      this._history.push({scroll, temporary: false})
       this._currentIndex = 0
       return
     }
@@ -47,7 +47,7 @@ class ViewerHistory {
       if (this.last()) {
         this.last().temporary = false
       }
-      this._history.push({scroll: scroll, temporary: false})
+      this._history.push({scroll, temporary: false})
       if (this.length() > 30) {
         this._history = this._history.slice(this.length() - 30)
       }
@@ -126,12 +126,12 @@ function decodePath(b64url) {
   return decodeURIComponent(s)
 }
 
-let query = document.location.search.substring(1)
-let parts = query.split('&')
+const query = document.location.search.substring(1)
+const parts = query.split('&')
 let encodedPdfFilePath
 let pdfFilePath
 for (let i = 0, ii = parts.length; i < ii; ++i) {
-    let param = parts[i].split('=')
+    const param = parts[i].split('=')
     if (param[0].toLowerCase() === 'file') {
         encodedPdfFilePath = param[1].replace(pdfFilePrefix, '')
         pdfFilePath = decodePath(encodedPdfFilePath)
@@ -144,36 +144,36 @@ for (let i = 0, ii = parts.length; i < ii; ++i) {
         }
     }
 }
-let server = `ws://${window.location.hostname}:${window.location.port}`
+const server = `ws://${window.location.hostname}:${window.location.port}`
 
-let socket = new WebSocket(server)
-socket.addEventListener("open", () => socket.send(JSON.stringify({type:"open", path:pdfFilePath, viewer:(embedded ? "tab" : "browser")})))
-socket.addEventListener("message", (event) => {
-    let data = JSON.parse(event.data)
+const socket = new WebSocket(server)
+socket.addEventListener('open', () => socket.send(JSON.stringify({type:'open', path:pdfFilePath, viewer:(embedded ? 'tab' : 'browser')})))
+socket.addEventListener('message', (event) => {
+    const data = JSON.parse(event.data)
     switch (data.type) {
-        case "synctex":
+        case 'synctex':
             // use the offsetTop of the actual page, much more accurate than multiplying the offsetHeight of the first page
-            let container = document.getElementById('viewerContainer')
-            var pos = PDFViewerApplication.pdfViewer._pages[data.data.page - 1].viewport.convertToViewportPoint(data.data.x, data.data.y)
-            let page = document.getElementsByClassName('page')[data.data.page - 1]
-            let scrollX = page.offsetLeft + pos[0]
-            let scrollY = page.offsetTop + page.offsetHeight - pos[1]
+            const container = document.getElementById('viewerContainer')
+            const pos = PDFViewerApplication.pdfViewer._pages[data.data.page - 1].viewport.convertToViewportPoint(data.data.x, data.data.y)
+            const page = document.getElementsByClassName('page')[data.data.page - 1]
+            const scrollX = page.offsetLeft + pos[0]
+            const scrollY = page.offsetTop + page.offsetHeight - pos[1]
 
             // set positions before and after SyncTeX to viewerHistory
             viewerHistory.set(container.scrollTop)
             container.scrollTop = scrollY - document.body.offsetHeight * 0.4
             viewerHistory.set(container.scrollTop)
 
-            let indicator = document.getElementById('synctex-indicator')
+            const indicator = document.getElementById('synctex-indicator')
             indicator.className = 'show'
             indicator.style.left = `${scrollX}px`
             indicator.style.top = `${scrollY}px`
             setTimeout(() => indicator.className = 'hide', 10)
             break
-        case "refresh":
+        case 'refresh':
             // Note: without showPreviousViewOnLoad = false restoring the position after the refresh will fail if
             // the user has clicked on any link in the past (pdf.js will automatically navigate to that link).
-            socket.send(JSON.stringify({type:"position", path:pdfFilePath,
+            socket.send(JSON.stringify({type:'position', path:pdfFilePath,
                                         scale:PDFViewerApplication.pdfViewer.currentScaleValue,
                                         scrollMode:PDFViewerApplication.pdfViewer.scrollMode,
                                         spreadMode:PDFViewerApplication.pdfViewer.spreadMode,
@@ -194,7 +194,7 @@ socket.addEventListener("message", (event) => {
               }, 2000);
             });
             break
-        case "position":
+        case 'position':
             PDFViewerApplication.pdfViewer.currentScaleValue = data.scale
             PDFViewerApplication.pdfViewer.scrollMode = data.scrollMode
             PDFViewerApplication.pdfViewer.spreadMode = data.spreadMode
@@ -204,7 +204,7 @@ socket.addEventListener("message", (event) => {
             viewerHistory._history = data.viewerHistory.history
             viewerHistory._currentIndex = data.viewerHistory.currentIndex
             break
-        case "params":
+        case 'params':
             if (data.scale) {
               PDFViewerApplication.pdfViewer.currentScaleValue = data.scale
             }
@@ -241,10 +241,10 @@ socket.onclose = () => { document.title = `[Disconnected] ${document.title}` }
 document.addEventListener('pagesinit', () => {
   // check whether WebSocket is open (readyState === 1).
   if (socket.readyState === 1) {
-    socket.send(JSON.stringify({type:"loaded", path:pdfFilePath}))
+    socket.send(JSON.stringify({type:'loaded', path:pdfFilePath}))
   } else {
-    socket.addEventListener("open", () => {
-      socket.send(JSON.stringify({type:"loaded", path:pdfFilePath}))
+    socket.addEventListener('open', () => {
+      socket.send(JSON.stringify({type:'loaded', path:pdfFilePath}))
     }, {once: true})
   }
 })
@@ -252,8 +252,8 @@ document.addEventListener('pagesinit', () => {
 // if we're embedded we cannot open external links here. So we intercept clicks and forward them to the extension
 if (embedded) {
   document.addEventListener('click', (e) => {
-      if (e.target.nodeName == 'A' && !e.target.href.startsWith(window.location.href)) { // is external link
-        socket.send(JSON.stringify({type:"external_link", url:e.target.href}))
+      if (e.target.nodeName === 'A' && !e.target.href.startsWith(window.location.href)) { // is external link
+        socket.send(JSON.stringify({type:'external_link', url:e.target.href}))
         e.preventDefault();
       }
   })
@@ -283,7 +283,7 @@ document.addEventListener('pagerendered', (evPageRendered) => {
         // no spread
         if (PDFViewerApplication.pdfViewer.spreadMode === 0) {
           viewerContainer = target.parentNode.parentNode
-        } 
+        }
         // odd and even spread add an extra spread container
         else {
           viewerContainer = target.parentNode.parentNode.parentNode
@@ -298,8 +298,8 @@ document.addEventListener('pagerendered', (evPageRendered) => {
           left += offsetLeft
         }
         const pos = PDFViewerApplication.pdfViewer._pages[page-1].getPagePoint(left, canvas_dom.offsetHeight - top)
-        socket.send(JSON.stringify({type:"click", path:pdfFilePath, pos:pos, page:page,
-         textBeforeSelection:textBeforeSelection, textAfterSelection:textAfterSelection}))
+        socket.send(JSON.stringify({type:'click', path:pdfFilePath, pos, page,
+         textBeforeSelection, textAfterSelection}))
     }
 }, true)
 
@@ -310,22 +310,22 @@ const setHistory = () => {
   setTimeout(() => {viewerHistory.set(container.scrollTop)}, 500)
 }
 
-document.getElementById('viewerContainer').addEventListener("click", setHistory)
-document.getElementById('sidebarContainer').addEventListener("click", setHistory)
+document.getElementById('viewerContainer').addEventListener('click', setHistory)
+document.getElementById('sidebarContainer').addEventListener('click', setHistory)
 
 // back button (mostly useful for the embedded viewer)
-document.getElementById("historyBack").addEventListener("click", function() {
+document.getElementById('historyBack').addEventListener('click', () => {
   viewerHistory.back()
 })
 
-document.getElementById("historyForward").addEventListener("click", function() {
+document.getElementById('historyForward').addEventListener('click', () => {
   viewerHistory.forward()
 })
 
 // keyboard bindings
-window.addEventListener('keydown', function(evt) {
+window.addEventListener('keydown', (evt) => {
   // F opens find bar, cause Ctrl-F is handled by vscode
-  if(evt.keyCode == 70 && evt.target.nodeName != 'INPUT') { // ignore F typed in the search box
+  if(evt.keyCode === 70 && evt.target.nodeName !== 'INPUT') { // ignore F typed in the search box
     showToolbar(false)
     PDFViewerApplication.findBar.open()
     evt.preventDefault()
@@ -334,9 +334,9 @@ window.addEventListener('keydown', function(evt) {
   // Chrome's usual Alt-Left/Right (Command-Left/Right on OSX) for history
   // Back/Forward don't work in the embedded viewer, so we simulate them.
   if (embedded && (evt.altKey || evt.metaKey)) {
-    if (evt.keyCode == 37) {
+    if (evt.keyCode === 37) {
       viewerHistory.back();
-    } else if(evt.keyCode == 39) {
+    } else if(evt.keyCode === 39) {
       viewerHistory.forward();
     }
   }
@@ -347,7 +347,7 @@ function showToolbar(animate) {
   if (hideToolbarInterval) {
     clearInterval(hideToolbarInterval)
   }
-  var d = document.getElementsByClassName('toolbar')[0]
+  const d = document.getElementsByClassName('toolbar')[0]
   d.className = d.className.replace(' hide', '') + (animate ? '' : ' notransition')
 
   hideToolbarInterval = setInterval(() => {
@@ -365,30 +365,30 @@ document.getElementById('outerContainer').onmousemove = (e) => {
   }
 }
 
-var currentUserSelectScale = undefined;
-var originalUserSelectIndex = undefined;
+let currentUserSelectScale = undefined;
+let originalUserSelectIndex = undefined;
 
 const getTrimScale = () => {
-  var trimSelect = document.getElementById("trimSelect");
+  const trimSelect = document.getElementById('trimSelect');
   if (trimSelect.selectedIndex <= 0) {
     return 1.0;
   }
-  var trimValue = trimSelect.options[trimSelect.selectedIndex].value;
+  const trimValue = trimSelect.options[trimSelect.selectedIndex].value;
   return 1.0/(1 - 2*trimValue);
 };
 
-document.getElementById("trimSelect").addEventListener("change", (ev) => {
+document.getElementById('trimSelect').addEventListener('change', (ev) => {
   const trimScale = getTrimScale();
-  const trimSelect = document.getElementById("trimSelect");
-  const scaleSelect = document.getElementById("scaleSelect");
-  const e = new Event("change");
+  const trimSelect = document.getElementById('trimSelect');
+  const scaleSelect = document.getElementById('scaleSelect');
+  const e = new Event('change');
   let o;
   if (trimSelect.selectedIndex <= 0) {
     for ( o of scaleSelect.options ) {
       o.disabled = false;
     }
-    document.getElementById("trimOption").disabled = true;
-    document.getElementById("trimOption").hidden = true;
+    document.getElementById('trimOption').disabled = true;
+    document.getElementById('trimOption').hidden = true;
     if (originalUserSelectIndex !== undefined) {
       scaleSelect.selectedIndex = originalUserSelectIndex;
     }
@@ -416,7 +416,7 @@ document.getElementById("trimSelect").addEventListener("change", (ev) => {
   if (originalUserSelectIndex === undefined) {
     originalUserSelectIndex = scaleSelect.selectedIndex;
   }
-  o = document.getElementById("trimOption");
+  o = document.getElementById('trimOption');
   o.value = currentUserSelectScale * trimScale;
   o.selected = true;
   scaleSelect.dispatchEvent(e);
@@ -424,12 +424,12 @@ document.getElementById("trimSelect").addEventListener("change", (ev) => {
 
 const trimPage = (page) => {
   const trimScale = getTrimScale();
-  const textLayer = page.getElementsByClassName("textLayer")[0];
-  const canvasWrapper = page.getElementsByClassName("canvasWrapper")[0];
-  const canvas = page.getElementsByTagName("canvas")[0];
+  const textLayer = page.getElementsByClassName('textLayer')[0];
+  const canvasWrapper = page.getElementsByClassName('canvasWrapper')[0];
+  const canvas = page.getElementsByTagName('canvas')[0];
   if ( !canvasWrapper || !canvas ) {
-    if (page.style.width !== "250px") {
-      page.style.width = "250px";
+    if (page.style.width !== '250px') {
+      page.style.width = '250px';
     }
     return;
   }
@@ -438,7 +438,7 @@ const trimPage = (page) => {
   if (m) {
     // add -4px to ensure that no horizontal scroll bar appears.
     const widthNum = Math.floor(Number(m[1])/trimScale) - 4;
-    const width = widthNum  + 'px';
+    const width = widthNum + 'px';
     page.style.width = width;
     canvasWrapper.style.width = width;
     const offsetX = - Number(m[1]) * (1 - 1/trimScale) / 2;
@@ -466,28 +466,28 @@ const trimPage = (page) => {
   }
 }
 
-window.addEventListener("pagerendered", () => {
-  const container = document.getElementById("trimSelectContainer");
-  const select = document.getElementById("trimSelect");
+window.addEventListener('pagerendered', () => {
+  const container = document.getElementById('trimSelectContainer');
+  const select = document.getElementById('trimSelect');
   container.setAttribute('style', 'display: inherit;');
   if (container.clientWidth > 0) {
     select.setAttribute('style', 'min-width: inherit;');
-    var width = select.clientWidth + 8;
+    const width = select.clientWidth + 8;
     select.setAttribute('style', 'min-width: ' + (width + 22) + 'px;');
     container.setAttribute('style', 'min-width: ' + width + 'px; ' + 'max-width: ' + width + 'px;');
   }
   if (select.selectedIndex <= 0) {
       return;
   }
-  const viewer = document.getElementById("viewer");
-  for( let page of viewer.getElementsByClassName("page") ){
+  const viewer = document.getElementById('viewer');
+  for( const page of viewer.getElementsByClassName('page') ){
     trimPage(page);
   }
 });
 
 const setObserverToTrim = () => {
   const observer = new MutationObserver(records => {
-    const trimSelect = document.getElementById("trimSelect");
+    const trimSelect = document.getElementById('trimSelect');
     if (trimSelect.selectedIndex <= 0) {
         return;
     }
@@ -496,8 +496,8 @@ const setObserverToTrim = () => {
       trimPage(page);
     })
   })
-  const viewer = document.getElementById("viewer");
-  for( let page of viewer.getElementsByClassName("page") ){
+  const viewer = document.getElementById('viewer');
+  for( const page of viewer.getElementsByClassName('page') ){
     if (page.dataset.isObserved !== 'observed') {
       observer.observe(page, {attributes: true, childList: true, attributeFilter: ['style']});
       page.setAttribute('data-is-observed', 'observed');
@@ -507,7 +507,7 @@ const setObserverToTrim = () => {
 
 // We need to recaluculate scale and left offset for trim mode on each resize event.
 window.addEventListener('resize', () =>{
-  const trimSelect = document.getElementById("trimSelect");
+  const trimSelect = document.getElementById('trimSelect');
   const ind = trimSelect.selectedIndex;
   if (!trimSelect || ind <= 0) {
     return;
