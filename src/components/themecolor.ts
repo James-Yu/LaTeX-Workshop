6122215e3b01848638f1545828ae681b91caac8d
Takashi Tamura <tamuratak@users.noreply.github.com>
2019-08-02 22:30:59 +0900

    use themeColorMap if getColor fails.

---
+++ src/components/themecolor.ts
@@ -0,0 +1,17 @@
+export const themeColorMap = {
+    'Abyss': 'dark',
+    'Default Dark+': 'dark',
+    'Default Light+': 'light',
+    'Visual Studio Dark': 'dark',
+    'Visual Studio Light': 'light',
+    'Default High Contrast': 'dark',
+    'Kimbie Dark': 'dark',
+    'Monokai Dimmed': 'dark',
+    'Monokai': 'dark',
+    'Quiet Light': 'light',
+    'Red': 'dark',
+    'vs-seti': 'dark',
+    'Solarized Dark': 'dark',
+    'Solarized Light': 'light',
+    'Tomorrow Night Blue': 'dark'
+}
