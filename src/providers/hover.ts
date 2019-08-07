533eacc3a1acca87a6110048cae271cf21cf3e3c
James-Yu <yujianqiaojames@gmail.com>
2019-08-07 12:21:49 +0800

    Forgot to change numberAtLastCompilation in context

--- src/providers/hover.ts
+++ src/providers/hover.ts
@@ -239,7 +239,7 @@ export class HoverProvider implements vscode.HoverProvider {
         const md = '```latex\n' + refData.label + '\n```\n'
         const refRange = document.getWordRangeAtPosition(position, /\{.*?\}/)
         const refNumberMessage = this.refNumberMessage(refData)
-        if (refNumberMessage !== undefined && configuration.get('hover.ref.numberAtLastCompilation.enabled') as boolean) {
+        if (refNumberMessage !== undefined && configuration.get('hover.ref.number.enabled') as boolean) {
             return new vscode.Hover([md, refNumberMessage, mdLink], refRange)
         }
         return new vscode.Hover([md, mdLink], refRange)
@@ -250,7 +250,7 @@ export class HoverProvider implements vscode.HoverProvider {
         const scale = configuration.get('hover.preview.scale') as number
 
         let tag: string
-        if (refData.prevIndex !== undefined && configuration.get('hover.ref.numberAtLastCompilation.enabled') as boolean) {
+        if (refData.prevIndex !== undefined && configuration.get('hover.ref.number.enabled') as boolean) {
             tag = refData.prevIndex.refNumber
         } else {
             tag = refData.label
