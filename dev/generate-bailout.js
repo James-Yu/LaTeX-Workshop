d7d526662df26eeb650bc33d935dc88d5bb338ed
Jerome Lelong <jerome.lelong@gmail.com>
2019-09-12 17:33:04 +0200

    Use local cpp grammar with bailout pattern
    This closes #1689.

---
+++ dev/generate-bailout.js
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-var-requires, @typescript-eslint/no-require-imports */
+const duplicateForEmbedding = require('textmate-bailout')
+
+duplicateForEmbedding({
+    // url for json-version of a tmLanguage
+    url: 'https://raw.githubusercontent.com/jeff-hykin/cpp-textmate-grammar/master/syntaxes/cpp.tmLanguage.json',
+    appendScope: 'latex',
+    bailoutPattern: '\\\\end\\{minted\\}',
+    newFileLocation: '../syntax/cpp-grammar-bailout.tmLanguage.json'
+})
