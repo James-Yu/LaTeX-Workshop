923403ccb041caac9b22bdf7947f6cb8d3ba42c6
Takashi Tamura <tamuratak@users.noreply.github.com>
2019-10-25 19:16:40 +0900

    stop using @jimp/custom

--- src/providers/preview/graphicsscaler_worker.ts
+++ src/providers/preview/graphicsscaler_worker.ts
@@ -1,23 +1,10 @@
 import * as workerpool from 'workerpool'
 
-import * as configure0 from '@jimp/custom'
-import * as bmp from '@jimp/bmp'
-import * as png from '@jimp/png'
-import * as jpeg from '@jimp/types'
-import * as resize from '@jimp/plugin-resize'
-import * as scal from '@jimp/plugin-scale'
-
-const configure = configure0 as any
-const j = configure({
-  types: [bmp, jpeg, png],
-  plugins: [resize, scal]
-})
-
 // workaround to avoid enabling esModuleInterop in tsconfig.json
 // If esModuleInterop enabled, some other packages do not work.
 import JimpT from 'jimp'
-//import * as JimpLib0 from 'jimp'
-const JimpLib = j as unknown as JimpT
+import * as JimpLib0 from 'jimp'
+const JimpLib = JimpLib0 as unknown as JimpT
 
 async function scale(filePath: string, opts: { height: number, width: number }): Promise<string> {
   const image = await JimpLib.read(filePath)
