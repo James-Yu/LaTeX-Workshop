30be904a5d24a205a7bbda3158f173de550e6db4
Takashi Tamura <tamuratak@users.noreply.github.com>
2019-12-01 08:31:58 +0900

    use ChildProcessWithoutNullStreams to assure that stdios are not null.

--- src/components/linter.ts
+++ src/components/linter.ts
@@ -1,7 +1,7 @@
 import * as vscode from 'vscode'
 import * as path from 'path'
 import * as fs from 'fs'
-import {ChildProcess, spawn, SpawnOptions} from 'child_process'
+import {ChildProcessWithoutNullStreams, spawn, SpawnOptionsWithoutStdio} from 'child_process'
 import {EOL} from 'os'
 
 import {Extension} from '../main'
@@ -9,7 +9,7 @@ import {Extension} from '../main'
 export class Linter {
     extension: Extension
     linterTimeout?: NodeJS.Timer
-    currentProcesses: {[linterId: string]: ChildProcess} = {}
+    currentProcesses: {[linterId: string]: ChildProcessWithoutNullStreams} = {}
 
     constructor(extension: Extension) {
         this.extension = extension
@@ -132,7 +132,7 @@ export class Linter {
         this.extension.logParser.parseLinter(stdout)
     }
 
-    processWrapper(linterId: string, command: string, args: string[], options: SpawnOptions, stdin?: string): Promise<string> {
+    processWrapper(linterId: string, command: string, args: string[], options: SpawnOptionsWithoutStdio, stdin?: string): Promise<string> {
         this.extension.logger.addLogMessage(`Linter for ${linterId} running command ${command} with arguments ${args}`)
         return new Promise((resolve, reject) => {
             if (this.currentProcesses[linterId]) {
@@ -141,16 +141,16 @@ export class Linter {
             const startTime = process.hrtime()
             this.currentProcesses[linterId] = spawn(command, args, options)
             const proc = this.currentProcesses[linterId]
-            proc.stdout?.setEncoding('binary')
-            proc.stderr?.setEncoding('binary')
+            proc.stdout.setEncoding('binary')
+            proc.stderr.setEncoding('binary')
 
             let stdout = ''
-            proc.stdout?.on('data', newStdout => {
+            proc.stdout.on('data', newStdout => {
                 stdout += newStdout
             })
 
             let stderr = ''
-            proc.stderr?.on('data', newStderr => {
+            proc.stderr.on('data', newStderr => {
                 stderr += newStderr
             })
 
@@ -171,12 +171,12 @@ export class Linter {
             })
 
             if (stdin !== undefined) {
-                proc.stdin?.write(stdin)
+                proc.stdin.write(stdin)
                 if (!stdin.endsWith(EOL)) {
                     // Always ensure we end with EOL otherwise ChkTeX will report line numbers as off by 1.
-                    proc.stdin?.write(EOL)
+                    proc.stdin.write(EOL)
                 }
-                proc.stdin?.end()
+                proc.stdin.end()
             }
         })
     }
