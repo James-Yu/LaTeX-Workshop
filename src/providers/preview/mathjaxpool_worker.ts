45e8fa1b49a6975a17640f1be2ce18d2907652b3
Takashi Tamura <tamuratak@users.noreply.github.com>
2019-12-20 21:10:28 +0900

    fix the types of workers.

--- src/providers/preview/mathjaxpool_worker.ts
+++ src/providers/preview/mathjaxpool_worker.ts
@@ -43,6 +43,9 @@ export async function typeset(arg: any, opts: { scale: number, color: string }):
 
 const workers = {typeset}
 
-export type IMathJaxWorker = typeof workers
+// workerpool passes the resolved value of Promise, not Promise.
+export type IMathJaxWorker = {
+    typeset: (...args: Parameters<typeof typeset>) => string
+}
 
 workerpool.worker(workers)
