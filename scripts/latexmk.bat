a6da903b6ccc75917f9d61768383b5439bbf5fd7
MagicLegend <jort@elska.nl>
2020-02-09 20:41:04 +0100

    Fixed naming oopsie

--- scripts/latexmk.bat
+++ scripts/latexmk.bat
@@ -1,1 +1,1 @@
-@docker -H %LATEXWORKSHOP_DOCKER_LOCATION% run -i --rm -w /data -v "%cd%:/data" %LATEXWORKSHOP_DOCKER_LATEX% latexmk %*
+@docker -H %LATEXWORKSHOP_DOCKER_DAEMON_LOCATION% run -i --rm -w /data -v "%cd%:/data" %LATEXWORKSHOP_DOCKER_LATEX% latexmk %*
--- scripts/texcount.bat
+++ scripts/texcount.bat
@@ -1,1 +1,1 @@
-@docker -H %LATEXWORKSHOP_DOCKER_LOCATION% run -i --rm -w /data -v "%cd%:/data" %LATEXWORKSHOP_DOCKER_LATEX% texcount %*
+@docker -H %LATEXWORKSHOP_DOCKER_DAEMON_LOCATION% run -i --rm -w /data -v "%cd%:/data" %LATEXWORKSHOP_DOCKER_LATEX% texcount %*
