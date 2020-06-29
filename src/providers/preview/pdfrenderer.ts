d2b51891ef34105c8be2605366e445a965e1d631
Takashi Tamura <tamuratak@users.noreply.github.com>
2020-06-30 08:32:30 +0900

    Make properites private or readonly.

--- src/providers/preview/pdfrenderer.ts
+++ src/providers/preview/pdfrenderer.ts
@@ -1,16 +1,13 @@
-import {Extension} from '../../main'
 import * as path from 'path'
 import * as workerpool from 'workerpool'
 import {Proxy} from 'workerpool'
 import {IPdfRendererWorker} from './pdfrenderer_worker'
 
 export class PDFRenderer {
-    extension: Extension
-    pool: workerpool.WorkerPool
-    proxy: workerpool.Promise<Proxy<IPdfRendererWorker>>
+    private readonly pool: workerpool.WorkerPool
+    private readonly proxy: workerpool.Promise<Proxy<IPdfRendererWorker>>
 
-    constructor(extension: Extension) {
-        this.extension = extension
+    constructor() {
         this.pool = workerpool.pool(
             path.join(__dirname, 'pdfrenderer_worker.js'),
             { maxWorkers: 1, workerType: 'process' }
