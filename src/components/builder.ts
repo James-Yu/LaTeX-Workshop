0c36fdfda156b0c52dc74dbd612ed78e4997462d
Takashi Tamura <tamuratak@users.noreply.github.com>
2020-09-17 08:42:55 +0900

    Tweak a log message. The phrase "the current shell" is misleading.

--- src/components/builder.ts
+++ src/components/builder.ts
@@ -314,7 +314,7 @@ export class Builder {
         this.currentProcess.on('error', err => {
             this.extension.logger.addLogMessage(`LaTeX fatal error: ${err.message}, ${stderr}. PID: ${pid}.`)
             this.extension.logger.addLogMessage(`Does the executable exist? PATH: ${process.env.PATH}`)
-            this.extension.logger.addLogMessage(`The current SHELL: ${process.env.SHELL}`)
+            this.extension.logger.addLogMessage(`The environment variable $SHELL: ${process.env.SHELL}`)
             this.extension.logger.displayStatus('x', 'errorForeground', `Recipe terminated with fatal error: ${err.message}.`, 'error')
             this.currentProcess = undefined
             this.extension.buildInfo.buildEnded()
