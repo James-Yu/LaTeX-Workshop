486d474c08becf9ab96a74ca7f7c0551772babe7
Jerome Lelong <jerome.lelong@gmail.com>
2021-01-10 07:01:16 +0100

    Define a configurable clean command

--- src/components/cleaner.ts
+++ src/components/cleaner.ts
@@ -2,6 +2,7 @@ import * as vscode from 'vscode'
 import * as path from 'path'
 import * as fs from 'fs-extra'
 import glob from 'glob'
+import * as cs from 'cross-spawn'
 
 import type {Extension} from '../main'
 
@@ -22,6 +23,22 @@ export class Cleaner {
                 return
             }
         }
+        const configuration = vscode.workspace.getConfiguration('latex-workshop')
+        const cleanMethod = configuration.get('latex.clean.method') as string
+        switch (cleanMethod) {
+            case 'glob':
+                return this.cleanGlob(rootFile)
+                break
+            case 'cleanCommand':
+                return this.cleanCommand(rootFile)
+                break
+            default:
+                this.extension.logger.addLogMessage(`Unknown cleaning method: ${cleanMethod}`)
+                return
+        }
+    }
+
+    async cleanGlob(rootFile: string): Promise<void> {
         const configuration = vscode.workspace.getConfiguration('latex-workshop')
         let globs = configuration.get('latex.clean.fileTypes') as string[]
         const outdir = path.resolve(path.dirname(rootFile), this.extension.manager.getOutDir(rootFile))
@@ -69,5 +86,41 @@ export class Cleaner {
                 }
             })
         })
+    }
+
+    cleanCommand(rootFile: string): Promise<void> {
+        const configuration = vscode.workspace.getConfiguration('latex-workshop')
+        const command = configuration.get('latex.clean.command') as string
+        let args = configuration.get('latex.clean.args') as string[]
+        if (args) {
+            args = args.map(arg => arg.replace('%TEX%', rootFile))
+        }
+        this.extension.logger.addLogMessage(`Clean temporary files using: ${command}, ${args}`)
+        const proc = cs.spawn(command, args, {cwd: path.dirname(rootFile), detached: true})
+
+        let stderr = ''
+        proc.stderr.on('data', newStderr => {
+            stderr += newStderr
+        })
+
+        return new Promise( (resolve, reject) => {
+            proc.on('error', err => {
+                this.extension.logger.addLogMessage(`Cannot run ${command}: ${err.message}, ${stderr}`)
+                if (err instanceof Error) {
+                    this.extension.logger.logError(err)
+                }
+                reject(err)
+            })
+            proc.on('exit', exitCode => {
+                if (exitCode === 0) {
+                    resolve()
+                } else {
+                    this.extension.logger.addLogMessage(`The clean command failed with exit code ${exitCode}`)
+                    this.extension.logger.addLogMessage(`Clean command stderr: ${stderr}`)
+                }
+            })
+
+        })
+
     }
 }
