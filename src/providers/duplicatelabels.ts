33268ebb2c65ac09a3864373ff0b2bdbdf8363ab
Jerome Lelong <jerome.lelong@gmail.com>
2021-02-24 17:26:22 +0100

    Simplify the computation of duplicates
    Do not cache the result, it hardly spares computations.
    We fully recompute the duplicates every time.

--- src/providers/duplicatelabels.ts
+++ src/providers/duplicatelabels.ts
@@ -3,12 +3,10 @@ import * as path from 'path'
 
 import type {Extension} from '../main'
 
-type LabelEntries = Map<string, {file: string, range: vscode.Range}[]>
 
 export class DuplicateLabels {
     private readonly extension: Extension
     private readonly duplicatedLabelsDiagnostics = vscode.languages.createDiagnosticCollection('Duplicate Labels')
-    private readonly labelEntries: LabelEntries = new Map()
 
     constructor(extension: Extension) {
         this.extension = extension
@@ -23,80 +21,71 @@ export class DuplicateLabels {
     /**
      * Compute the dictionary of labels holding their file and position
      */
-    private parseFileLabels(file: string) {
+    private computeDuplicates(file: string): string[] {
         if (! (file in this.extension.manager.cachedContent)) {
             this.extension.logger.addLogMessage(`Cannot check for duplicate labels in a file not in manager: ${file}.`)
-            return
+            return []
         }
-        const cachedRefs = this.extension.manager.cachedContent[file].element.reference
-        if (cachedRefs === undefined) {
-            return
-        }
-        cachedRefs.forEach(ref => {
-            if (ref.range === undefined) {
+        const labelsCount: Map<string, number> = new Map()
+        this.extension.manager.getIncludedTeX().forEach(cachedFile => {
+            const cachedRefs = this.extension.manager.cachedContent[cachedFile].element.reference
+            if (cachedRefs === undefined) {
                 return
             }
-            let locations = this.labelEntries.get(ref.label)
-            if (locations === undefined) {
-                locations = []
-                this.labelEntries.set(ref.label, locations)
-            }
-            locations.push({
-                file,
-                range: ref.range instanceof vscode.Range ? ref.range : ref.range.inserting
+            cachedRefs.forEach(ref => {
+                if (ref.range === undefined) {
+                    return
+                }
+                let count = labelsCount.get(ref.label)
+                if (count === undefined) {
+                    count = 0
+                }
+                count += 1
+                labelsCount.set(ref.label, count)
             })
         })
-    }
-
-    /**
-     * Delete all labels
-     *  - that were attached to file
-     *  - that are attached to a file not in getTeXChildren()
-     */
-    private deleteOldLabels(file: string) {
-        const allFiles = this.extension.manager.getIncludedTeX()
-        for (const label of this.labelEntries.keys()) {
-            const locations = this.labelEntries.get(label)
-            if (locations !== undefined) {
-                const newLocations = locations.filter(entry => {
-                    return (entry.file !== file) && allFiles.includes(entry.file)
-                })
-                if (newLocations.length === 0) {
-                    this.labelEntries.delete(label)
-                } else {
-                    this.labelEntries.set(label, newLocations)
-                }
+        const duplicates = []
+        for (const [label, count] of labelsCount) {
+            if (count > 1) {
+                duplicates.push(label)
             }
         }
+        return duplicates
     }
 
-    /**
-     * Only update the previous diagnostic based on the content of `file`
-     */
     run(file: string) {
         this.extension.logger.addLogMessage(`Checking for duplicate labels: ${file}.`)
-        this.deleteOldLabels(file)
-        this.parseFileLabels(file)
-        this.showDiagnostics()
+        const duplicates = this.computeDuplicates(file)
+        this.showDiagnostics(duplicates)
     }
 
-    private showDiagnostics() {
+    private showDiagnostics(duplicates: string[]) {
         this.duplicatedLabelsDiagnostics.clear()
+        if (duplicates.length === 0) {
+            return
+        }
         const diagsCollection: { [key: string]: vscode.Diagnostic[] } = {}
 
-        for (const [label, locations] of this.labelEntries.entries()) {
-            if (locations.length < 2) {
-                continue
+        this.extension.manager.getIncludedTeX().forEach(cachedFile => {
+            const cachedRefs = this.extension.manager.cachedContent[cachedFile].element.reference
+            if (cachedRefs === undefined) {
+                return
             }
-            for (const loc of locations) {
-                if (!(loc.file in diagsCollection)) {
-                    diagsCollection[loc.file] = []
+            cachedRefs.forEach(ref => {
+                if (ref.range === undefined) {
+                    return
                 }
-                const diag = new vscode.Diagnostic(loc.range, `Duplicate label ${label}`, vscode.DiagnosticSeverity.Warning)
-                diag.source = 'DuplicateLabels'
-                diagsCollection[loc.file].push(diag)
-            }
-        }
+                if (duplicates.includes(ref.label)) {
+                   if (! (cachedFile in diagsCollection)) {
+                        diagsCollection[cachedFile] = []
+                        const range = ref.range instanceof vscode.Range ? ref.range : ref.range.inserting
+                        const diag = new vscode.Diagnostic(range, `Duplicate label ${ref.label}`, vscode.DiagnosticSeverity.Warning)
+                        diag.source = 'DuplicateLabels'
+                        diagsCollection[cachedFile].push(diag)
+                   }
+                }
+            })
+        })
 
         for (const file in diagsCollection) {
             if (path.extname(file) === '.tex') {
@@ -106,7 +95,6 @@ export class DuplicateLabels {
     }
 
     reset() {
-        this.labelEntries.clear()
         this.duplicatedLabelsDiagnostics.clear()
     }
 }
