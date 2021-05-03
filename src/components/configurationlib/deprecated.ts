4b418f4c02913af750423226695a879c61a0a59f
Takashi Tamura <tamuratak@users.noreply.github.com>
2021-05-03 19:50:44 +0900

    Make deprecatedConfigurations empty.

--- src/components/configurationlib/deprecated.ts
+++ src/components/configurationlib/deprecated.ts
@@ -10,92 +10,7 @@ type DeprecatedConfig = {
 export class DeprecatedConfiguration {
     private readonly extension: Extension
 
-    private readonly deprecatedConfigurations: DeprecatedConfig[] = [
-        {
-            oldConfigKey: 'latex-workshop.maxPrintLine.option.enabled',
-            newConfigKey: 'latex-workshop.latex.option.maxPrintLine.enabled'
-        },
-        {
-            oldConfigKey: 'latex-workshop.chktex.interval',
-            newConfigKey: 'latex-workshop.chktex.delay'
-        },
-        {
-            oldConfigKey: 'latex-workshop.latex.outputDir',
-            newConfigKey: 'latex-workshop.latex.outDir'
-        },
-        {
-            oldConfigKey: 'latex-workshop.view.autoActivateLatex.enabled',
-            newConfigKey: 'latex-workshop.view.autoFocus.enabled'
-        },
-        {
-            oldConfigKey: 'latex-workshop.hoverPreview.enabled',
-            newConfigKey: 'latex-workshop.hover.preview.enabled'
-        },
-        {
-            oldConfigKey: 'latex-workshop.hoverReference.enabled',
-            newConfigKey: 'latex-workshop.hover.ref.enabled'
-        },
-        {
-            oldConfigKey: 'latex-workshop.hoverCitation.enabled',
-            newConfigKey: 'latex-workshop.hover.citation.enabled'
-        },
-        {
-            oldConfigKey: 'latex-workshop.hoverCommandDoc.enabled',
-            newConfigKey: 'latex-workshop.hover.command.enabled'
-        },
-        {
-            oldConfigKey: 'latex-workshop.hoverPreview.scale',
-            newConfigKey: 'latex-workshop.hover.preview.scale'
-        },
-        {
-            oldConfigKey: 'latex-workshop.hoverPreview.cursor.enabled',
-            newConfigKey: 'latex-workshop.hover.preview.cursor.enabled'
-        },
-        {
-            oldConfigKey: 'latex-workshop.hoverPreview.cursor.symbol',
-            newConfigKey: 'latex-workshop.hover.preview.cursor.symbol'
-        },
-        {
-            oldConfigKey: 'latex-workshop.hoverPreview.cursor.color',
-            newConfigKey: 'latex-workshop.hover.preview.cursor.color'
-        },
-        {
-            oldConfigKey: 'latex-workshop.hoverPreview.ref.enabled',
-            newConfigKey: 'latex-workshop.hover.ref.enabled'
-        },
-        {
-            oldConfigKey: 'latex-workshop.latex.clean.enabled',
-            newConfigKey: 'latex-workshop.latex.autoClean.run'
-        },
-        {
-            oldConfigKey: 'latex-workshop.latex.clean.onFailBuild.enabled',
-            newConfigKey: 'latex-workshop.latex.autoClean.run'
-        },
-        {
-            oldConfigKey: 'latex-workshop.latex.autoBuild.onSave.enabled',
-            newConfigKey: 'latex-workshop.latex.autoBuild.run'
-        },
-        {
-            oldConfigKey: 'latex-workshop.latex.autoBuild.onTexChange.enabled',
-            newConfigKey: 'latex-workshop.latex.autoBuild.run'
-        },
-        {
-            oldConfigKey: 'latex-workshop.hover.ref.numberAtLastCompilation.enabled',
-            newConfigKey: 'latex-workshop.hover.ref.number.enabled'
-        },
-        {
-            oldConfigKey: 'latex-workshop.latex-workshop.view.pdf.tab.useNewGroup',
-            newConfigKey: 'latex-workshop.view.pdf.tab.editorGroup'
-        },
-        {
-            oldConfigKey: 'latex-workshop.view.pdf.external.command',
-            newConfigKey: 'latex-workshop.view.pdf.external.viewer.command'
-        },
-        {
-            oldConfigKey: 'latex-workshop.intellisense.preview.enabled',
-            newConfigKey: 'latex-workshop.intellisense.includegraphics.preview.enabled'
-        }
-    ]
+    private readonly deprecatedConfigurations: DeprecatedConfig[] = []
 
     constructor(extension: Extension) {
         this.extension = extension
@@ -108,9 +23,14 @@ export class DeprecatedConfiguration {
             if (hasConf) {
                 let msg: string
                 if (conf.newConfigKey) {
-                    msg = `"${conf.oldConfigKey}" has been replaced by "${conf.newConfigKey}". Please manually remove the deprecated config from your settings.`
+                    msg = `"${conf.oldConfigKey}" has been replaced by "${conf.newConfigKey}".`
                 } else {
-                    msg = `"${conf.oldConfigKey}" has been deprecated. Please manually remove the deprecated config from your settings.`
+                    msg = `"${conf.oldConfigKey}" has been deprecated.`
+                }
+                if (conf.message) {
+                    msg = msg + ' ' + conf.message
+                } else {
+                    msg = msg + ' ' + 'Please manually remove the deprecated config from your settings.'
                 }
                 this.extension.logger.addLogMessage(`Deprecated configuration is used: ${conf.oldConfigKey}`)
                 this.extension.logger.displayStatus('check', 'statusBar.foreground', msg, 'warning')
