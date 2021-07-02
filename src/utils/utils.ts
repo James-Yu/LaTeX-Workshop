bf9850ba1bc5f1fea4cdf4a3965f7a8e8c8564aa
Jerome Lelong <jerome.lelong@gmail.com>
2021-07-02 12:58:17 +0200

    Use import type

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,7 +2,7 @@ import * as vscode from 'vscode'
 import * as path from 'path'
 import * as fs from 'fs'
 
-import {latexParser} from 'latex-utensils'
+import type {latexParser} from 'latex-utensils'
 
 
 export function sleep(ms: number) {
