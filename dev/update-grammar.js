00db24e00d16df4208c2d572c4a8e14e843afb86
Jerome Lelong <jerome.lelong@gmail.com>
2021-12-23 10:35:25 +0100

    Use vscode-extend-language to update grammars and language configurations

--- dev/update-grammar.js
+++ dev/update-grammar.js
@@ -1,45 +1,17 @@
-const got = require('got')
 const fs = require('fs')
 const path = require('path')
-
-async function getRequest(url, options) {
-    try {
-        const response = await got(url, options)
-        return response.body
-    } catch (e) {
-        console.log('Error code:', e.response.statusCode)
-        console.log('Error message:', e.response.statusMessage);
-    }
-    return undefined
-}
+const vel = require('vscode-extend-language')
 
 async function downloadFile(repo, file, version='main') {
     const url = 'https://raw.githubusercontent.com/' + repo + '/' + version + '/' + file
-    var res = await getRequest(url)
-    if (res === undefined) {
+    var content = await vel.download(url)
+    if (!content) {
         console.log('Cannot retrieve ', url)
         return
     }
-    const content = res.toString('utf-8')
     const syntaxFilePath = '../syntax/' + path.basename(file)
     fs.writeFileSync(syntaxFilePath, content)
-    console.log('Updating ', syntaxFilePath)
-}
-
-async function getCommitSha(repo, version='main') {
-    const lastCommitInfo = 'https://api.github.com/repos/' + repo + '/git/ref/heads/' + version
-    var res = await getRequest(lastCommitInfo, {headers: {'User-Agent': 'vscode-latex-basics'}})
-    if (res === undefined) {
-        console.log('Cannot retrieve last commit sha', lastCommitInfo)
-        return
-    }
-    try {
-        const lastCommit = JSON.parse(res.toString('utf-8'))
-        const sha = lastCommit.object.sha
-        console.log(`Update grammar files to ${repo}@${sha}`)
-    } catch(e) {
-        console.log('Cannot read last commit information')
-    }
+    console.log('Updating', syntaxFilePath)
 }
 
 async function main() {
@@ -52,7 +24,12 @@ async function main() {
         'markdown-latex-combined.tmLanguage.json'
     ]
 
-    await getCommitSha(latexBasicsRepo)
+    const sha = await vel.getCommitSha(latexBasicsRepo)
+    if (sha) {
+        console.log(`Update grammar files to ${latexBasicsRepo}@${sha}`)
+    } else {
+        console.log('Cannot read last commit information')
+    }
     for (const file of grammarFiles) {
         downloadFile(latexBasicsRepo, 'syntaxes/' + file)
     }
