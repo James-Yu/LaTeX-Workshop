7db24ba28cc3e9a4f1c1d0ef5efaa01968505c1f
Takashi Tamura <tamuratak@users.noreply.github.com>
2022-02-24 12:37:16 +0900

    Use VS Code 1.64.2 for tests

--- test/runTest.ts
+++ test/runTest.ts
@@ -45,7 +45,7 @@ async function runTestsOnEachFixture(targetName: 'build' | 'rootfile' | 'viewer'
     for (const testWorkspace of testBuildWorkspaces) {
         const nodejsTimeout = setTimeout(() => process.exit(1), firstTime ? 3*60000 : 60000)
         await runTests({
-            version: '1.62.0',
+            version: '1.64.2',
             extensionDevelopmentPath,
             extensionTestsPath,
             launchArgs: [
