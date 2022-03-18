fd5e0220052afb766632af1c741028cf613fc900
Jerome Lelong <jerome.lelong@gmail.com>
2022-03-18 22:01:20 +0100

    Test all .json files for completion

--- test/unittest.test.ts
+++ test/unittest.test.ts
@@ -1,12 +1,32 @@
 import * as assert from 'assert'
 import * as process from 'process'
 import * as vscode from 'vscode'
+import * as fs from 'fs'
+import * as path from 'path'
+import * as glob from 'glob'
 
-import {stripComments} from '../src/utils/utils'
 
 import {
     runTestWithFixture,
+    checkDictkeys as checkDictKeys
 } from './utils'
+
+type EnvType = {
+    name: string,
+    snippet?: string,
+    detail?: string,
+    package?: string
+}
+
+type CmdType = {
+    command: string,
+    snippet?: string,
+    documentation?: string,
+    package?: string,
+    detail?: string,
+    postAction?: string,
+    label?: string
+}
 
 suite('unit test suite', () => {
 
@@ -18,11 +38,53 @@ suite('unit test suite', () => {
         return
     })
 
-    runTestWithFixture('fixture001', 'basic unit tests', () => {
-        assert.strictEqual(
-            stripComments('abc % comment'),
-            'abc'
-        )
+    runTestWithFixture('fixture001', 'check default environment .json completion file', () => {
+        const extensionRoot = path.resolve(__dirname, '../../')
+        const file = `${extensionRoot}/data/environments.json`
+        const envs = JSON.parse(fs.readFileSync(file, {encoding: 'utf8'})) as {[key: string]: EnvType}
+        assert.ok(Object.keys(envs).length > 0)
+        Object.keys(envs).forEach(name => {
+            assert.ok(checkDictKeys(Object.keys(envs[name]), ['name'] , ['snippet', 'detail']), file + ': ' + JSON.stringify(envs[name]))
+        })
         return Promise.resolve()
     })
+
+    runTestWithFixture('fixture001', 'check environments from package .json completion files', () => {
+        const extensionRoot = path.resolve(__dirname, '../../')
+        const files = glob.sync('data/packages/*_env.json', {cwd: extensionRoot})
+        files.forEach(file => {
+            const envs = JSON.parse(fs.readFileSync(path.join(extensionRoot, file), {encoding: 'utf8'})) as {[key: string]: EnvType}
+            assert.ok(Object.keys(envs).length > 0)
+            Object.keys(envs).forEach(name => {
+                assert.ok(checkDictKeys(Object.keys(envs[name]), ['name', 'snippet', 'detail', 'package']), file + ': ' + JSON.stringify(envs[name]))
+            })
+        })
+        return Promise.resolve()
+    })
+
+    runTestWithFixture('fixture001', 'check default commands .json completion file', () => {
+        const extensionRoot = path.resolve(__dirname, '../../')
+        const file = `${extensionRoot}/data/commands.json`
+        const cmds = JSON.parse(fs.readFileSync(file, {encoding: 'utf8'})) as {[key: string]: CmdType}
+        assert.ok(Object.keys(cmds).length > 0)
+        Object.keys(cmds).forEach(name => {
+            assert.ok(checkDictKeys(Object.keys(cmds[name]), ['command'] , ['snippet', 'documentation', 'detail', 'postAction', 'label']), file + ': ' + JSON.stringify(cmds[name]))
+        })
+        return Promise.resolve()
+    })
+
+    runTestWithFixture('fixture001', 'check commands from package .json completion files', () => {
+        const extensionRoot = path.resolve(__dirname, '../../')
+        const files = glob.sync('data/packages/*_cmd.json', {cwd: extensionRoot})
+        files.forEach(file => {
+            const cmds = JSON.parse(fs.readFileSync(path.join(extensionRoot, file), {encoding: 'utf8'})) as {[key: string]: CmdType}
+            assert.ok(Object.keys(cmds).length > 0)
+            Object.keys(cmds).forEach(name => {
+                assert.ok(checkDictKeys(Object.keys(cmds[name]), ['command', 'snippet'] , ['documentation', 'detail', 'postAction', 'package', 'label']), file + ': ' + JSON.stringify(cmds[name]))
+            })
+        })
+        return Promise.resolve()
+    })
+
+
 })
