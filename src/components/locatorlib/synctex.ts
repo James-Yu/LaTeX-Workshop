d48d2c7e8e14d66f3c3d88338ea622222498ff53
James Yu <yujianqiaojames@gmail.com>
2023-01-09 17:00:53 +0800

    Update synctex.ts

--- src/components/locatorlib/synctex.ts
+++ src/components/locatorlib/synctex.ts
@@ -95,6 +95,10 @@ export class SyncTexJs {
         } catch (e: unknown) {
             logger.logError(`Failed parsing .synctex.gz ${synctexFileGz} .`, e)
         }
+        
+        if (!fs.existsSync(synctexFile) && !fs.existsSync(synctexFileGz)) {
+            throw new SyncTexJsError(`${synctexFile}, ${synctexFileGz} not found.`)
+        }
 
         throw new SyncTexJsError('SyncTeX failed.')
     }
