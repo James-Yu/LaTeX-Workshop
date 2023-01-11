7d2e98ba05e75c84e924acfdeee3fd7610faa283
James-Yu <yujianqiaojames@gmail.com>
2023-01-11 10:32:58 +0800

    Dev viewer patch script is modified accordingly

--- dev/editviewer.py
+++ dev/editviewer.py
@@ -12,8 +12,8 @@ with open(args.web + '/viewer.html', 'rt') as fin:
             fout.write(
                 line.replace('''<title>PDF.js viewer</title>''', '''<meta http-equiv="Content-Security-Policy" content="default-src 'self'; base-uri 'none'; connect-src 'self' ws://127.0.0.1:*; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob:;">\n    <title>PDF.js viewer</title>''')
                     .replace('''<link rel="stylesheet" href="viewer.css">''', '''<link rel="stylesheet" href="viewer.css">\n    <link rel="stylesheet" href="latexworkshop.css">''')
-                    .replace('''<script src="../build/pdf.js"></script>''', '''<script src="/build/pdf.js" defer></script>''')
-                    .replace('''<script src="viewer.js"></script>''', '''<script src="/out/viewer/latexworkshop.js" type="module"></script>''')
+                    .replace('''<script src="../build/pdf.js"></script>''', '''<script src="build/pdf.js" defer></script>''')
+                    .replace('''<script src="viewer.js"></script>''', '''<script src="out/viewer/latexworkshop.js" type="module"></script>''')
             )
 
 with open(args.web + '/viewer.js', 'rt') as fin:
