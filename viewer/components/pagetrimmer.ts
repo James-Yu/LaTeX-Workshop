d1079b213fe386ac62793b29bb1866b7ef8877d9
James-Yu <yujianqiaojames@gmail.com>
2023-03-03 10:38:30 +0800

    Prevent popup text annotation from overflow

--- viewer/components/pagetrimmer.ts
+++ viewer/components/pagetrimmer.ts
@@ -1,6 +1,23 @@
 import type { ILatexWorkshopPdfViewer } from './interface.js'
 
 export function getTrimScale() {
+    const viewer = document.getElementById('viewer') as HTMLElement
+    return Number(viewer.style.getPropertyValue('--trim-factor'))
+}
+
+export function registerPageTrimmer(lwApp: ILatexWorkshopPdfViewer) {
+    lwApp.onPagesLoaded(() => {
+        resizeDOM()
+        repositionDOM()
+        setTrimScale()
+    })
+    const select = document.getElementById('trimSelect') as HTMLElement
+    select.addEventListener('change', setTrimScale)
+}
+
+window.addEventListener('resize', resizeDOM)
+
+function calcTrimScale() {
     const trimSelect = document.getElementById('trimSelect') as HTMLSelectElement
     if (trimSelect.selectedIndex <= 0) {
         return 1.0
@@ -9,32 +26,30 @@ export function getTrimScale() {
     return 1.0/(1 - 2*Number(trimValue))
 }
 
-export function registerPageTrimmer(lwApp: ILatexWorkshopPdfViewer) {
-    lwApp.onPagesLoaded(() => {
-        initCSS()
-        trimPages()
-    });
-    (document.getElementById('trimSelect') as HTMLElement).addEventListener('change', trimPages)
-}
-
-function trimPages() {
-    const trimScale = getTrimScale()
+function setTrimScale() {
+    const trimScale = calcTrimScale()
     const viewer = document.getElementById('viewer') as HTMLElement
     viewer.style.setProperty('--trim-factor', `${trimScale}`)
 }
 
-window.addEventListener('resize', initCSS)
-
-function initCSS() {
+function resizeDOM() {
     const viewer = document.getElementById('viewer') as HTMLElement
-    resizeDOM(viewer)
-}
-
-function resizeDOM(viewer: HTMLElement) {
     for (const page of viewer.getElementsByClassName('page') as HTMLCollectionOf<HTMLElement>){
         page.style.setProperty('--unit-width', (page.style.width.match(/[0-9]+/) || ['0'])[0] + 'px')
         page.style.setProperty('--unit-height', (page.style.height.match(/[0-9]+/) || ['0'])[0] + 'px')
         // calc(var(--scale-factor) * 792px)
         page.style.height = page.style.height.replace('px)', 'px * var(--trim-factor))')
+    }
+}
+
+function repositionDOM() {
+    for (const anno of document.getElementsByClassName('textAnnotation') as HTMLCollectionOf<HTMLElement>) {
+        if (parseFloat(anno.style.left) <= 50) {
+            continue
+        }
+        for (const popup of anno.getElementsByClassName('popupWrapper') as HTMLCollectionOf<HTMLElement>) {
+            popup.style.right = '100%'
+            popup.style.left = ''
+        }
     }
 }
