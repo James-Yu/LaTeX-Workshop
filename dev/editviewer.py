d0264ecb22cf520a798c5309e82f525187aebfe7
Pascal Drinkuth <uk070552@student.uni-kassel.de>
2023-04-14 23:51:44 +0200

    reworked added function logic to better understand the code
    added 'console.error(`webviewerloaded:' replace back
    
    doubled replace for better removeability
    removed the edit of the webViewerLoaded function
    
    
    removed dublicate

--- dev/editviewer.py
+++ dev/editviewer.py
@@ -5,7 +5,6 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-w', '--web', help=f'Path to pdf.js distributed web/ folder, end without `/`', type=str)
 parser.add_argument('-v', '--viewer', help=f'Path to extension viewer/ folder, end without `/`', type=str)
 args = parser.parse_args()
-webViewerLoaded=False
 
 with open(args.web + '/viewer.html', 'rt') as fin:
     with open(args.viewer + '/viewer.html', 'wt') as fout:
@@ -20,32 +19,21 @@ with open(args.web + '/viewer.html', 'rt') as fin:
 with open(args.web + '/viewer.js', 'rt') as fin:
     with open(args.viewer + '/viewer.js', 'wt') as fout:
         for line in fin:
-            if line == "function webViewerLoad() {\n":
-                webViewerLoaded=True
-            if webViewerLoaded:
-                fout.write(
-                        line.replace('''const event = document.createEvent("CustomEvent");''','''const event = new CustomEvent("webviewerloaded", {''')
-                            .replace('''event.initCustomEvent("webviewerloaded", true, true, {''','''bubbles: true,cancelable: true,detail: {''')
-                            .replace('''source: window''','''source: window}''')
-                            .replace('''  try {''','''  document.dispatchEvent(event);\ntry {''')
-                            .replace('''console.error(`webviewerloaded: ''', '''// console.error(`webviewerloaded: ''')
-                )
-            else:
-                fout.write(
-                    line.replace('''this.setTitle(title);''', '''// this.setTitle(title);''')
-                        .replace('''const MATCH_SCROLL_OFFSET_TOP = -50;''', '''const MATCH_SCROLL_OFFSET_TOP = -100;''')
-                        .replace('''this.switchView(view, true);''', '''this.switchView(view, false);''')
-                        .replace('''this.removePageBorders = options.removePageBorders || false;''', '''this.removePageBorders = options.removePageBorders || true;''')
-                        .replace('''localStorage.setItem("pdfjs.history", databaseStr);''', '''// localStorage.setItem("pdfjs.history", databaseStr);''')
-                        .replace('''return localStorage.getItem("pdfjs.history");''', '''return // localStorage.getItem("pdfjs.history");''')
-                        .replace('''localStorage.setItem("pdfjs.preferences", JSON.stringify(prefObj));''', '''// localStorage.setItem("pdfjs.preferences", JSON.stringify(prefObj));''')
-                        .replace('''return JSON.parse(localStorage.getItem("pdfjs.preferences"));''', '''return // JSON.parse(localStorage.getItem("pdfjs.preferences"));''')
-                        .replace('''console.warn('#' + key + ' is undefined.');''', '''// console.warn('#' + key + ' is undefined.');''')
-                        .replace('''(!event.shiftKey || window.chrome || window.opera)) {''', '''(!event.shiftKey || window.chrome || window.opera)) {\n    if (window.parent !== window) {\n      return;\n    }''')
-                        .replace('''console.log(`PDF ${pdfDocument.''', '''// console.log(`PDF ${pdfDocument.''')
-                        .replace('''pdfjsLib = require("../build/pdf.js");''','''pdfjsLib = require("./build/pdf.js");''')
-                        .replace('''value: "../build/pdf.worker.js",''', '''  value: "./build/pdf.worker.js",''')
-                        .replace('''console.log(`PDF ${pdfDocument.''', '''// console.log(`PDF ${pdfDocument.''')
+            fout.write(
+                line.replace('''this.setTitle(title);''', '''// this.setTitle(title);''')
+                    .replace('''const MATCH_SCROLL_OFFSET_TOP = -50;''', '''const MATCH_SCROLL_OFFSET_TOP = -100;''')
+                    .replace('''this.switchView(view, true);''', '''this.switchView(view, false);''')
+                    .replace('''this.removePageBorders = options.removePageBorders || false;''', '''this.removePageBorders = options.removePageBorders || true;''')
+                    .replace('''localStorage.setItem("pdfjs.history", databaseStr);''', '''// localStorage.setItem("pdfjs.history", databaseStr);''')
+                    .replace('''return localStorage.getItem("pdfjs.history");''', '''return // localStorage.getItem("pdfjs.history");''')
+                    .replace('''localStorage.setItem("pdfjs.preferences", JSON.stringify(prefObj));''', '''// localStorage.setItem("pdfjs.preferences", JSON.stringify(prefObj));''')
+                    .replace('''return JSON.parse(localStorage.getItem("pdfjs.preferences"));''', '''return // JSON.parse(localStorage.getItem("pdfjs.preferences"));''')
+                    .replace('''console.warn('#' + key + ' is undefined.');''', '''// console.warn('#' + key + ' is undefined.');''')
+                    .replace('''(!event.shiftKey || window.chrome || window.opera)) {''', '''(!event.shiftKey || window.chrome || window.opera)) {\n    if (window.parent !== window) {\n      return;\n    }''')
+                    .replace('''console.error(`webviewerloaded: ''', '''// console.error(`webviewerloaded: ''')
+                    .replace('''console.log(`PDF ${pdfDocument.''', '''// console.log(`PDF ${pdfDocument.''')
+                    .replace('''pdfjsLib = require("../build/pdf.js");''','''pdfjsLib = require("./build/pdf.js");''')
+                    .replace('''value: "../build/pdf.worker.js",''', '''  value: "./build/pdf.worker.js",''')
                 )
 
 os.system(f'git diff --no-index {args.web}/viewer.html {args.viewer}/viewer.html > {args.viewer}/../dev/viewer/viewer.html.diff')
