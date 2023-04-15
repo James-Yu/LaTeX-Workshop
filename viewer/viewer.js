b8e41914ea037da4c6152d7a6761ddecbe04b346
Pascal Drinkuth <uk070552@student.uni-kassel.de>
2023-04-15 12:08:01 +0200

    applied patch

--- viewer/viewer.js
+++ viewer/viewer.js
@@ -753,7 +753,7 @@ const defaultOptions = {
     kind: OptionKind.WORKER
   },
   workerSrc: {
-    value: "../build/pdf.worker.js",
+      value: "./build/pdf.worker.js",
     kind: OptionKind.WORKER
   }
 };
@@ -3670,7 +3670,7 @@ let pdfjsLib;
 if (typeof window !== "undefined" && window["pdfjs-dist/build/pdf"]) {
   pdfjsLib = window["pdfjs-dist/build/pdf"];
 } else {
-  pdfjsLib = require("../build/pdf.js");
+  pdfjsLib = require("./build/pdf.js");
 }
 module.exports = pdfjsLib;
 
