0b73f85b10dccb57a57050a43f45df8ee6a46909
James Yu <yujianqiaojames@gmail.com>
2023-05-28 02:00:17 +0800

    Fix #3898, maybe

--- src/components/viewer.ts
+++ src/components/viewer.ts
@@ -99,11 +99,11 @@ export class Viewer {
         const tabEditorGroup = configuration.get('view.pdf.tab.editorGroup') as string
         const viewer = mode ?? configuration.get<'tab' | 'browser' | 'singleton' | 'external'>('view.pdf.viewer', 'tab')
         if (viewer === 'browser') {
-            return lw.viewer.openBrowser(lw.manager.tex2pdf(pdfFile))
+            return lw.viewer.openBrowser(pdfFile)
         } else if (viewer === 'tab' || viewer === 'singleton') {
-            return lw.viewer.openTab(lw.manager.tex2pdf(pdfFile), tabEditorGroup, true)
+            return lw.viewer.openTab(pdfFile, tabEditorGroup, true)
         } else if (viewer === 'external') {
-            return lw.viewer.openExternal(lw.manager.tex2pdf(pdfFile))
+            return lw.viewer.openExternal(pdfFile)
         }
     }
 
