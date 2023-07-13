dc282e940ccde89ee16498a974f8177b0157eda0
ManuelHentschel <53863351+ManuelHentschel@users.noreply.github.com>
2023-07-13 20:28:05 +0200

    Add option to include filename in status bar

--- src/components/builder.ts
+++ src/components/builder.ts
@@ -764,13 +764,24 @@ class BuildToolQueue {
     }
 
     getStepString(step: Step): string {
+        let stepString: string
         if (step.timestamp !== this.steps[0]?.timestamp && step.index === 0) {
-            return step.recipeName
+            stepString = step.recipeName
         } else if (step.timestamp === this.steps[0]?.timestamp) {
-            return `${step.recipeName}: ${step.index + 1}/${this.steps[this.steps.length - 1].index + 1} (${step.name})`
+            stepString = `${step.recipeName}: ${step.index + 1}/${this.steps[this.steps.length - 1].index + 1} (${step.name})`
         } else {
-            return `${step.recipeName}: ${step.index + 1}/${step.index + 1} (${step.name})`
+            stepString = `${step.recipeName}: ${step.index + 1}/${step.index + 1} (${step.name})`
         }
+        if(step.rootFile) {
+            const rootFileUri = vscode.Uri.file(step.rootFile)
+            const configuration = vscode.workspace.getConfiguration('latex-workshop', rootFileUri)
+            const showFilename = configuration.get<boolean>('latex.build.rootfileInStatus', false)
+            if(showFilename) {
+                const relPath = vscode.workspace.asRelativePath(step.rootFile)
+                stepString = `${relPath}: ${stepString}`
+            }
+        }
+        return stepString
     }
 
     getStep(): Step | undefined {
