d4541b7b8b24d454f2b8859a0011a0161bad67b9
James Yu <yujianqiaojames@gmail.com>
2024-06-26 15:10:23 +0100

    Fallback to English string in l10n

--- src/language/l10n.ts
+++ src/language/l10n.ts
@@ -3,18 +3,19 @@ import * as path from 'path'
 import { lw } from '../lw'
 
 let localeStrings: {[key: string]: string} | undefined
-async function retrieveLangString(): Promise<{[key: string]: string}> {
-    if (localeStrings !== undefined) {
-        return localeStrings
+let defaultStrings: {[key: string]: string} | undefined
+async function retrieveLangString() {
+    const langPath = path.resolve(lw.extensionRoot, `package.nls.${vscode.env.language}.json`)
+    if (await lw.file.exists(langPath)) {
+        localeStrings = JSON.parse(await lw.file.read(langPath) ?? '{}') as {[key: string]: string}
     }
-    let langPath = path.resolve(lw.extensionRoot, `package.nls.${vscode.env.language}.json`)
-    if (!await lw.file.exists(langPath)) {
-        langPath = path.resolve(lw.extensionRoot, 'package.nls.json')
-    }
-    localeStrings = JSON.parse(await lw.file.read(langPath) ?? '{}') as {[key: string]: string}
-    return localeStrings
+    const engPath = path.resolve(lw.extensionRoot, 'package.nls.json')
+    defaultStrings = JSON.parse(await lw.file.read(engPath) ?? '{}') as {[key: string]: string}
 }
 
 export async function getLocaleString(key: string): Promise<string> {
-    return (await retrieveLangString())[key] ?? key
+    if (defaultStrings === undefined) {
+        await retrieveLangString()
+    }
+    return localeStrings?.[key] ?? defaultStrings?.[key] ?? key
 }
