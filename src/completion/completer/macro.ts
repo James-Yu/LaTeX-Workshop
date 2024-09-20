248c52ddd284f035b29c469b04f38ef328464529
James Yu <yujianqiaojames@gmail.com>
2024-09-20 11:47:02 +0800

    Prioritize lower case over upper

--- src/completion/completer/macro.ts
+++ src/completion/completer/macro.ts
@@ -421,7 +421,7 @@ function entryCmdToCompletion(item: MacroRaw, packageName?: string, postAction?:
         suggestion.documentation += ` From package: ${packageName}.`
     }
     suggestion.sortText = (item.name + (item.arg?.format ?? ''))
-        .replace(/([A-Z])/g, '$10').toLowerCase()
+        .replace(/([a-z])/g, '$10').toLowerCase()
         .replaceAll('{', '0')
         .replaceAll('[', '1')
         .replaceAll('(', '2')
