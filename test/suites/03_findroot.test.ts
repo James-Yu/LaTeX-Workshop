a253abf0eac83dd540ecc1687d5bc028a9690bef
James Yu <yujianqiaojames@gmail.com>
2024-09-24 16:55:13 +0800

    We can now skip a few ci suites

--- test/suites/03_findroot.test.ts
+++ test/suites/03_findroot.test.ts
@@ -4,7 +4,7 @@ import * as assert from 'assert'
 import { lw } from '../../src/lw'
 import * as test from './utils'
 
-suite('Find root file test suite', () => {
+suite.skip('Find root file test suite', () => {
     test.suite.name = path.basename(__filename).replace('.test.js', '')
     test.suite.fixture = 'testground'
 
