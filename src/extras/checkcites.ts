f5f3781fd4cab7071d49d676d52683fff93afa8b
sukiElaina <112391611+sukiElaina@users.noreply.github.com>
2025-11-04 13:08:30 +0800

    Improve Checkcites can read the output dir

--- src/extras/checkcites.ts
+++ src/extras/checkcites.ts
@@ -1,12 +1,17 @@
+import * as vscode from 'vscode'
 import { EOL } from 'os'
 import { lw } from '../lw'
+import * as path from 'path'
 
 const logger = lw.log('Citations', 'Linter')
 
 export function checkCitations() {
   logger.log('Checking citations.')
-
-  const auxFile = lw.root.file.path?.replace(/\.tex$/, '.aux')
+  const configuration = vscode.workspace.getConfiguration('latex-workshop', lw.file.toUri(lw.root.file.path!))
+  const aux = lw.root.file.path?.replace(/\.tex$/, '.aux')
+  const auxDir = path.join(path.dirname(aux || ''),path.normalize(configuration.get('latex.outDir') as string))
+  const auxBaseName = aux ? path.basename(aux) : ''
+  const auxFile = aux ? path.join(auxDir, auxBaseName) : undefined
   if (!auxFile) {
     logger.log('No aux file found.')
     return []
